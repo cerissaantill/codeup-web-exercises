d6ae16e0f048e853f34460893b96360428af1d68
Cerissa Antill <cerissa.antill@gmail.com>
2019-02-07 15:22:51 -0600

    added to external.js and external_js.html

--- js/external.js
+++ js/external.js
@@ -13,7 +13,7 @@
 
 
 
-// 3. Write some JavaScript code, that is, variables and operators, to describe the following scenarios.
+// 1. Write some JavaScript code, that is, variables and operators, to describe the following scenarios.
 //    Do not worry about the real operations to get the values, the goal of these exercises is
 //    to understand how real world conditions can be represented with code.
 
@@ -41,8 +41,6 @@ alert("The total rental cost is " + "" + "$" + totalRentalCost).toFixed(2);
 
 
 
-
-
 // CONTRACTOR
 
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. ' +
@@ -63,8 +61,7 @@ totalPayment = googleHours * googleHourlyRateDollars;
 totalPayment = amazonHours * amazonHourlyRateDollars;
 totalPayment = facebookHours * facebookHourlyRateDollars;
 
-alert("Total wages erned: $" + totalPayment.toFixed(2));
-
+alert("Total wages earned: $" + totalPayment.toFixed(2));
 
 
 
@@ -85,6 +82,7 @@ alert("Student enrollment status: " + studentEnrolled);
 
 // PRODUCT OFFER
 
+
 // A product offer can be applied only if a person buys more than 2 items, and
 // the offer has not expired. Premium members do not need to buy a specific amount
 // of products.
@@ -93,14 +91,96 @@ var numberOfItems = prompt("Enter number of items bought"); //number
 var offerIsValid = confirm("Offer is valid"); //boolean
 var isPremiumMember = confirm("Is a premium member"); //boolean
 
-var productDiscountApplied = (isPremiumMember || Number(numberOfItems) > 2) && offerIsValid);
+var productDiscountApplied = (isPremiumMember || Number(numberOfItems) > 2) && offerIsValid;
 
 alert("Product discount status: " + productDiscountApplied);
 
 
+// ----------------------------------------------
+
+
+//-- FUNCTIONS --//
+
+// 1. Write a function, isShortWord, that takes in a string and returns the
+//    boolean value true if the passed argument is shorter than 5 characters.
+//    Return false otherwise.
+
+function isShortWord (string) {
+    return string.length < 5;
+}
+
+isShortWord("test1");
+isShortWord("cat");
 
 
 
+
+// 2. Write a function, isSameLength, that takes in two string inputs and
+//    returns the boolean value true if the passed arguments are the same
+//    length. Return false otherwise.
+
+function isSameLength(str1, str2) {
+    return str1.length === str2.length;
+}
+
+console.log(isSameLength("bob", "cat")); // true
+console.log(isSameLength("bob", "catasdf")); // false
+
+
+// 3. Write a function, getSmallerSegment, that takes in a string and a number
+//    input. The function should return a substring of the first argument that
+//    is as many characters long as the second argument in lowercase.
+
+//    example input: getSmallerSegment("Codeup", 3)
+//    example output: "cod"
+
+function getSmallerSegment (string, number) {
+    return string.substring(0, number).toLowerCase();
+}
+
+console.log(getSmallerSegment("Codeup, 3")); // "cod"
+console.log(getSmallerSegment("hello", 2)); // "he"
+
+
+// --------------------------------------
+
+
+//-- SCOPE --//
+
+// SIDE EFFECTS vs PURE FUNCTIONS
+
+
+// functions with side effects
+var word = "hello";
+
+function shoutWord(input) {
+    return word.toUpperCase();
+}
+
+console.log(shoutWord("bird"));
+
+
+// pure function
+
+// Immediately-Invoked Function Expression:
+// IIFE:
+
+
+// <html>
+// <body>
+// <script>
+// (function() {
+//
+//
+// (can place USE SCTRICT inside IFFE)
+//
+//
+// })();
+// The IIFE should wrap everything in the html doc
+//
+// </script>
+// </body>
+// </html>
 
 
 
