012ee80b09a050fb574d7efab1f153f861105a8e
Cerissa Antill <cerissa.antill@gmail.com>
2019-02-07 19:55:02 -0600

    added more exercises to external.js &  functions.js

--- js/external.js
+++ js/external.js
@@ -5,11 +5,11 @@
 
 // console.log("Hello from external Javascript!");
 
-// alert('Welcome to my Website!');
-//
-// var userColor = prompt("What is your favorite color?");
-//
-// alert(userColor + " is my favorite color too!");
+alert('Welcome to my Website!');
+
+var userColor = prompt("What is your favorite color?");
+
+alert(userColor + " is my favorite color too!");
 
 
 
