14a569c98aba37498669d5ed95aa2f0e539b441e
Cerissa Antill <cerissa.antill@gmail.com>
2019-02-14 11:03:02 -0600

    added arrays exercises and bonus problems

---
+++ bonus work/arrays-practice.js
@@ -0,0 +1,39 @@
+// =======================Loops and Array Bonuses=========================
+//     1.  create an array of shapes
+
+    var shapes = ['circle', 'square', 'triangle', 'rectangle'];
+
+
+// – prompt the user to search for a specific shape
+    prompt('Search for a shape: ');
+
+// – using a for loop, iterate through the array to log the shapes until the matching shape is found
+    for (i = 0; i < shapes; i++) {
+
+        const shapes = [
+            {name: 'circle', content: { }},
+            {name: 'square', }
+        ]
+
+
+        // shapes.indexOf('circle');
+        // shapes.indexOf('square');
+        // shapes.indexOf('triangle');
+        // shapes.indexOf('rectangle');
+        // console.log();
+    }
+
+// – once the shape is found, log a message (“Shape is found”) and use a break statement to exit loop.
+
+
+// 2.  Create a function that returns a random day of the week
+
+
+// 3.  Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
+// – someFunction("a") // returns 1
+// – someFunction("z") // returns 26
+// 4.  Create a function that returns the longest string in a given array of string elements.
+// 5.   Create a function that takes in two arrays of elements, including numbers. If all numbers added together in the first array is equal to all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
+// – var arr1 = ['bob', 1, true, 1, 2];
+// – var arr2 = [2, null, undefined, 0, 2, "apple"]
+// – exampleFunction(arr1, arr2) // returns true
