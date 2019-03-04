26dc2fb8553b064091f6caf20993ed4bf7053214
Cerissa Antill <cerissa.antill@gmail.com>
2019-03-04 11:58:39 -0600

    add traversal exercise to jquery_faq.html

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,6 @@
 
 
-src="http://code.jquery.com/jquery.js"
-integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+
+src="http://code.jquery.com/jquery-3.3.1.min.js"
+integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 crossorigin="anonymous"
