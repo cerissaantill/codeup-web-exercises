2bca51a098f609070d3d4c720c711a163c001ddd
Cerissa Antill <cerissa.antill@gmail.com>
2019-03-12 09:49:57 -0500

    solutions from walkthrough

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+// From index.js include your function defined in say-hello.js.
+
+"use strict";
+
+import message from "./say-hello.js";
+
+console.log(message.niceMessage());
+
+
+// import message from say-hello.js
+
+// we assigned the name message. could be math, banana, etc.
---
+++ src/say-hello.js
@@ -0,0 +1,16 @@
+
+// Create a file that exports a function
+// that returns a string with a nice message.
+
+"use strict";
+
+
+    const niceMessage = () => {
+        return 'Nice Message'
+    };
+
+    export default {niceMessage};
+
+
+
+// this exports a function that returns a nice message
